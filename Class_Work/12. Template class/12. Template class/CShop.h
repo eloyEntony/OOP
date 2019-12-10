41e6e4002b832c4e25a5657980a37c39b45bf3cf
AntonKozyar <kozyar.ant@gmail.com>
2019-12-10 19:20:05 +0200

    12.10.19

--- Class_Work/12. Template class/12. Template class/CShop.h
+++ Class_Work/12. Template class/12. Template class/CShop.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
 
@@ -12,43 +13,38 @@ class CShop
 	T *arr;
 
 public:
-	CShop() {};
+	CShop() {}
 
 	CShop(int size)
 	{
 		this->size = size;
 		this->arr = new T[this->size];
-		for (int i = 0; i < this->size; i++)
-		{
+		for (int i = 0; i < this->size; i++){
 			arr[i];
 		}
 	};
 
 	void Show_info()
 	{
-		for (int i = 0; i < this->size; i++)
-		{
+		for (int i = 0; i < this->size; i++){
 			arr[i].Show_info();
 		}
 
 		cout << endl;
-	};
+	}
 
 	void Most_expensive()
 	{
 		int max = this->arr[0].Get_price();
 
-		for (int i = 0; i < this->size; i++)
-		{
-			if (arr[i].Get_price() > max) {
+		for (int i = 0; i < this->size; i++){
+			if (arr[i].Get_price() > max){
 				max = arr[i].Get_price();
 			}
 		}
 
-		for (int i = 0; i < this->size; i++)
-		{
-			if (arr[i].Get_price() == max)
-			{
+		for (int i = 0; i < this->size; i++){	
+			if (arr[i].Get_price() == max){
 				cout << " Most expensive : ";
 					this->arr[i].Show_info();
 			}
@@ -66,11 +62,9 @@ public:
 			}
 		}
 
-		for (int i = 0; i < this->size; i++)
-		{
-			if (arr[i].Get_price() == min)
-			{
-				cout << " Cheapest_product : ";
+		for (int i = 0; i < this->size; i++){
+			if (arr[i].Get_price() == min){
+				cout << " Cheapest product : ";
 				this->arr[i].Show_info();
 			}
 		}
@@ -81,21 +75,46 @@ public:
 		float average_price=0;
 		int suma = 0;
 
-		for (int i = 0; i < this->size; i++)
-		{
+		for (int i = 0; i < this->size; i++){
 			suma += arr[i].Get_price();
 		}
 
 		average_price = suma / this->size;
-
 		cout << " Average price : " << average_price << endl;
 
+	}
+
+	void operator[](const int index) {
+		arr[index].Show_info();
+	}
+
+	void Sort_min_max()
+	{
+		cout << "^^^^^^ Sort_min_max ^^^^^^" << endl;
+		for (int i = this->size - 1; i > 1; i--){
+			for (int j = 0; j < i; j++){
+				if (arr[j].Get_price() > arr[j + 1].Get_price())
+					swap(arr[j], arr[j + 1]);
+			}
+		}		
+	}
+
+	void Sort_max_min() 
+	{
+		cout << "^^^^^^ Sort_max_min ^^^^^^" << endl;
+		for (int i = this->size - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++){
+				if (arr[j].Get_price() < arr[j + 1].Get_price())
+					swap(arr[j], arr[j + 1]);
+				
+			}
+		}	
 	}
 
 	~CShop()
 	{
 		arr = nullptr;
 		delete[] arr;
-	};
+	}
 };
 
--- Class_Work/12. Template class/12. Template class/Source.cpp
+++ Class_Work/12. Template class/12. Template class/Source.cpp
@@ -6,6 +6,36 @@
 #include"Megazine.h"
 using namespace std;
 
+
+/*You need to create a template class "CShop" (Store), which works like a regular template array of any type and contains :
+fields:
+pointer to array type;
+the size of the array.
+additional methods :
+search for the minimum and maximum element of the array;
+method for sorting elements of an array in ascending or descending order(for example, by price; by 12 points);
+method for finding the arithmetic mean of all elements of an array;
+overload indexing operator.
+You must also create the following set of classes :
+Book containing the following fields :
+book title;
+publishing house;
+author;
+price;
+a boolean field indicating whether the book is new: true - new, false - no.
+Megazine, which contains the following fields :
+the name of the journal;
+price;
+boolean field indicating whether the log is new: true - new, false - no.
+An audio disc(Audio) which should contain the following fields :
+disk name;
+performer;
+number of tracks;
+price;
+a boolean field indicating whether an audio disk is new: true - new, false - no.
+For each class write the necessary set of methods.
+In the main program, create 3 store template objects for the types that are valid in the program(Books, Magazine, Audio disks).*/
+
 int main() {
 	
 
@@ -17,6 +47,16 @@ int main() {
 	shop_b.Cheapest_product();
 	cout << "\n=============================================" << endl;
 	shop_b.Average_price();
+
+	cout << "----------------->>> Indexing operator";
+	shop_b[0];
+	
+	shop_b.Sort_min_max();
+	shop_b.Show_info();
+
+	shop_b.Sort_max_min();
+	shop_b.Show_info();
+	
 
 	/*CShop <Audio> shop_a(1);
 	shop_a.Show_info();
