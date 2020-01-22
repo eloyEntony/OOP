ff5a2d5f8129d83f08f2851297487ce049d9f27e
Anton Kozyar <Kozyar@rivne.itstep.org>
2020-01-22 14:53:06 +0200

    01.22.20

---
+++ Class_Work/24. Smart pointer/24. Smart pointer/Source.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+/*
+Дано рядок(С або С++ на вибір).
+Перевірити чи він містить вірно записану IP-адресу.
+Використати рядкові потоки.
+Наприклад, 255.255.255.0 - вірна, 255.255.0.256, 192.0.157 – невірні.
+*/
+
+class IP {
+
+	char arr[4];
+public:
+	void Fill() {
+		
+		for (int i = 0; i < 4; i++) {
+			cin >> arr[i];
+
+			if (arr[i] == 46) {
+				this->arr[4] = arr[i];
+				continue;
+			}
+		}
+	}
+
+	void Check() {
+
+		if (arr[0] != 46 && arr[4] == 46) {
+			cout << "GOOD" << endl;
+		}
+
+		
+	}
+
+};
+
+int main() {
+
+	IP *ip = new IP[4];
+
+	for (int i = 0; i < 4; i++){
+		ip[i].Fill();
+	}
+
+	for (int i = 0; i < 4; i++) {
+		ip[i].Check();
+	}
+
+	system("pause");
+	return 0;
+}
+
+
