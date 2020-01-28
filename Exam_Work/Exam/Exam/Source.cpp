f4622f1138e51a1464649761a7260aff18967d30
AntonKozyar <kozyar.ant@gmail.com>
2020-01-28 21:53:31 +0200

    01.28.20

--- Exam_Work/Exam/Exam/Source.cpp
+++ Exam_Work/Exam/Exam/Source.cpp
@@ -29,108 +29,111 @@ using namespace std;
 class Apartments {
 
 	int rooms;		// кімнати
-	float s;		//площа
-	int over;		//поверх
+	float s;		// площа
+	int over;		// поверх
 	string area;    // район
 
 public:
-
-	Apartments() {
+	
+	Apartments() {	//конструктор
 		Exchange_request();
 	};
 
-
-	void Exchange_request() { //заявкa на обмін
-
-		cout << "Create request >>> " << endl;
-		cout << " Enter room : ";
+	void Exchange_request() { //заявкa на обмін, заповнення екземпляру
+		
+		cout << "\n\t <<<Create request >>> \n" << endl;
+		cout << " Enter room   : ";
 		cin >> this->rooms;
-		cout << " Enter S : ";
+		cout << " Enter S      : ";
 		cin >> this->s;
-		cout << " Enter over : ";
+		cout << " Enter over   : ";
 		cin >> this->over;
-		cout << " Enter area : ";
+		cout << " Enter area   : ";
 		cin.ignore();
 		getline(cin, this->area);
-
 	}
-
-
 
 	void Show() {
-		cout << "\n---------------------------------" << endl;
-		cout << " ROOM : " << this->rooms << endl;
-		cout << " S    : " << this->s << endl;
-		cout << " OVER : " << this->over << endl;
-		cout << " AREA : " << this->area << endl;
-		cout << "---------------------------------" << endl;
+		cout << "---------------------------------" << endl;		
+		cout << " ROOM  : " << this->rooms << endl;
+		cout << " S     : " << this->s << endl;
+		cout << " OVER  : " << this->over << endl;
+		cout << " AREA  : " << this->area << endl;
+		//cout << "---------------------------------" << endl;
 	}
 
-	~Apartments() {};
+	int Get_room() { return this->rooms; }
+	int Get_over() { return this->over; }
+	float Get_s() { return this->s; }
 
-	int Get_room() {
-		return this->rooms;
-	}
-
-	int Get_over() {
-		return this->over;
-	}
-
-	float Get_s() {
-		return this->s;
-	}
-
-	
+	~Apartments() {};	
 };
 
 
-void Serch(vector<Apartments> first , vector<Apartments> second) {	
-
+void Serch(vector<Apartments> &first) {
+	Apartments my_card;
+	bool exit = false;
 	for (int i = 0; i < first.size(); i++) {
-
-		float temp = first[i].Get_s()*0.10;
-
-		if ((first[i].Get_room() == second[i].Get_room()) && (first[i].Get_over() == second[i].Get_over()) && (second[i].Get_s() >= (first[i].Get_s() - temp) && second[i].Get_s() <= (first[i].Get_s() + temp))) {
-			first[i].Show();
-		}
-		else
-			cout << "NOO" << endl;
+		float temp = first[i].Get_s() * 0.10;
+		if ((first[i].Get_room() == my_card.Get_room()) && (first[i].Get_over() == my_card.Get_over()) && ((my_card.Get_s() >= (first[i].Get_s() - temp)) && (my_card.Get_s() <= (first[i].Get_s() + temp)))) 
+		{													//пошук схожого екземпляра класу
+			cout << " >>>[ DELETE ]<<< " << endl;			
+			first.erase(first.begin()+i);					// якщо є схожий тоді з вектора видаляється
+			exit = true;									// викідає з циклу якщо найшло зхожий
+			break;
+		}					
 	}
+	if (!exit) {
+	cout << " >>>[ ADD ]<<< " << endl;
+	first.push_back(my_card);		//якщо схожого немає, цей що шукали додається у вектор
+	}
+}
 
+
+void Menu() {
+	vector<Apartments> cartoteka;	
+	int choise;
+	bool exit = false;
+
+	while (!exit) {
+
+		cout << " ------------------- CARTOTEKA --------------------" << endl;
+		cout << "\n ~[1] CREATE \n ~[2] SEARCH\n ~[3] SHOW\n ~[0] EXIT\n" << endl;
+		cout << " Enter your choise : ";
+		cin >> choise;
+		
+		switch (choise)
+		{
+			case 1:			
+				cout << "\n ------------------- CREATE --------------------" << endl;
+				cout << " Enter count : ";
+				cin >> choise;
+				for (int i = 0; i < choise; i++) { cartoteka.push_back(Apartments()); }	//створення відповідної кількості контейнерів вектора
+				break;
+
+			case 2:
+				cout << "\n ------------------- SEARCH --------------------" << endl;				
+				Serch(cartoteka);
+				break;
+
+			case 3:
+				cout << "\n ------------------- SHOW --------------------" << endl;
+				for (int i = 0; i < cartoteka.size(); i++) {
+					cartoteka[i].Show();				//вивід вектора
+				}
+				break;
+
+			case 0:
+				exit = true;
+				break;
+
+			default:
+				break;
+		}
+	}	
 }
 
 int main() {
-
-	int room;
-	int over;
-	float s;
-
-	vector<Apartments> cartoteka;
-		
-	vector<Apartments>my_cartoteca;
-
-
-	cout << "\n---------------- CREATE -----------------" << endl;
-
-	cartoteka.push_back(Apartments());
-	
-	cout << "\n---------------- SEARCH -----------------" << endl;
-
-	my_cartoteca.push_back(Apartments());
-
-	/*cout << " Enter search room : ";
-	cin >> room;
-	cout << " Enter search over : ";
-	cin >> over;
-	cout << " Enter search S : ";
-	cin >> s;*/
-	
-	for (int i = 0; i < cartoteka.size(); i++) {
-
-		Serch(cartoteka, my_cartoteca);
-	}
-
-
-	system("pause");
+	Menu();	
 	return 0;
 }
