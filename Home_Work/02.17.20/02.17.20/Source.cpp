b82c4ca60df4dc99555bd88a5a7370a828ab024d
AntonKozyar <kozyar.ant@gmail.com>
2020-02-17 20:51:19 +0200

    02.17.20

--- Home_Work/02.17.20/02.17.20/Source.cpp
+++ Home_Work/02.17.20/02.17.20/Source.cpp
@@ -4,43 +4,122 @@
 using namespace std;
 
 //Завдання 1.  Використати шаблон Абстрактна фабрика.
-//Створити фабрики для створення кнопок та прапорців для ОС Windows, Macintosh  в залежності від вибору користувача.
+//Створити фабрики для створення кнопок та прапорців для ОС Windows, Macintosh  
+//в залежності від вибору користувача.
 //Створити клас Клієнт(Application) для перевірки роботи фабрик.
 //Діаграма класів подібно https ://refactoring.guru/uk/design-patterns/abstract-factory
 
 
 
-class Buttons_and_checkboxes {
-protected:
-	virtual void Show() const = 0;
-	virtual ~Buttons_and_checkboxes(){};
-};
-
-class Fabrica {
-protected:
-	virtual Buttons_and_checkboxes *Create()const = 0;
-};
-
-class Win_but : Buttons_and_checkboxes {
+class Buttons {
 public:
-	void Show() const override { cout << " Windows button X" << endl; }
+	virtual string Show() const = 0;
+	virtual ~Buttons(){};
 };
 
-class Mac_but : Buttons_and_checkboxes {
+class Win_but : public Buttons {
 public:
-	void Show() const override { cout << " MacOS button X" << endl; }
+	string Show() const override { return " Windows button"; }
 };
 
-class Windows : Fabrica {
-
+class Mac_but : public Buttons {
+public:
+	string Show() const override { return  " MacOS button "; }
 };
 
-class Mac : Fabrica {
 
+class Checkboxes {
+public:
+	virtual string Show() const = 0;
+	virtual ~Checkboxes() {};
 };
 
-int main()
-{
-	system("pause");
+class Win_box : public Checkboxes {
+public:
+	string Show() const override { return " Windows Checkboxes" ; }
+};
+
+class Mac_box : public Checkboxes {
+public:
+	string Show() const override { return " MacOS Checkboxes "; }
+};
+
+
+
+__interface Fabrica_abstruct {
+public:
+	virtual Buttons *Create_but()const = 0;
+	virtual Checkboxes *Create_box()const = 0;
+};
+
+
+class Windows : public Fabrica_abstruct {
+public:
+	Buttons *Create_but()const override {
+		return new Win_but();
+	}
+	Checkboxes *Create_box()const override {
+		return new Win_box();
+	}
+};
+
+class Mac : public Fabrica_abstruct {
+public:
+	Buttons *Create_but()const override {
+		return new Mac_but();
+	}
+	Checkboxes *Create_box()const override {
+		return new Mac_box();
+	}
+};
+
+class Application{
+public:
+	Application() {};
+	void Show(const Fabrica_abstruct &factory){
+		const Buttons * buttons = factory.Create_but();
+		const Checkboxes * checkboxes = factory.Create_box();
+
+		cout << "\n----------------\n ";
+		cout << buttons->Show();
+		cout << "\n ";
+		cout << checkboxes->Show() << endl;	
+
+		delete buttons;
+		delete checkboxes;
+	}
+	~Application() {};
+};
+
+
+
+int main(){
+	bool exit = false;
+	int choise;
+	Windows *w = new Windows();
+	Mac *m = new Mac();
+	Application app;	
+
+	while (!exit) {
+
+		cout << "\n----------------\n [1] Windows \n [2] Mac \n [0] Exit \n\n Enter : ";
+		cin >> choise;
+		switch (choise)
+		{
+			case 1:
+				app.Show(*w);
+				break;
+			case 2:
+				app.Show(*m);
+				break;
+			case 0:
+				delete w;
+				delete m;
+				exit = true;
+			default:
+				break;
+		}
+	}
+	
 	return 0;
 }
