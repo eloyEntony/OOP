c9e106e0d79940d4edbf38048393245a20ead5b5
Anton Kozyar <Kozyar@rivne.itstep.org>
2020-02-19 14:50:41 +0200

    02.19.20

---
+++ Class_Work/33. Pattern. State/33. Pattern. State/Source.cpp
@@ -0,0 +1,111 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+/*
+Скласти програму Світлофор з використанням патерну Стан(State).
+*/
+
+class Traffic_lights;
+
+class State {
+protected:
+	Traffic_lights * lights_;
+
+public:
+	virtual ~State(){}
+
+	void Set_lights(Traffic_lights * lights) {
+		this->lights_ = lights;
+	}
+	virtual void State1() = 0;
+	/*virtual void State2() = 0;
+	virtual void State3() = 0;*/
+};
+
+
+class Traffic_lights {
+private:
+	State *state_;
+
+public:
+	Traffic_lights(State *state) : state_(nullptr) {
+		this->Transition(state);
+	}
+
+	~Traffic_lights() {
+		delete state_;
+	}
+
+	void Transition(State *state) {
+		cout << "\n Traffic lights: Transition to " << typeid(*state).name();
+		if (this->state_ != nullptr)
+			delete this->state_;
+		this->state_ = state;
+		this->state_->Set_lights(this);
+	}
+
+	void Reqest1() {
+		this->state_->State1();
+	}
+	/*void Reqest2() {
+		this->state_->State2();
+	}
+	void Reqest3() {
+		this->state_->State3();
+	}*/
+
+};
+
+
+class Grean_color : public State {
+public:
+	void State1()override {
+		system("color 20");
+		cout << "\n Traffic lights <-> GREAN" << endl;		
+	}
+	//void State2()override {}
+	//void State3()override {}
+};
+class Yelow_color : public State {
+public:
+	//void State1()override {}
+	void State1()override {
+		system("color 60");
+		cout << "\n Traffic lights <-> YELOW" << endl;		
+	}
+	//void State3()override {}
+};
+
+class Red_color : public State {
+public:
+	/*void State1()override {}
+	void State2()override {}	*/
+	void State1()override {
+		system("color 40");
+		cout << "\n Traffic lights <-> RED" << endl;		
+	}
+};
+
+int main() {
+
+	Traffic_lights *context = new Traffic_lights(new Grean_color);
+	context->Reqest1();
+	//context->Reqest2();
+	//context->Reqest3();
+	system("pause");
+
+	context = new Traffic_lights(new Yelow_color);
+	context->Reqest1();
+    /*context->Reqest2();
+	context->Reqest3()*/;
+	system("pause");
+
+	context = new Traffic_lights(new Red_color);
+	context->Reqest1();
+	/*context->Reqest2();
+	context->Reqest3();*/
+	delete context;
+
+	system("pause");
+	return 0;
+}
---
+++ Class_Work/34. Pettern. Observer/34. Pettern. Observer/Source.cpp
@@ -0,0 +1,113 @@
+﻿#include<iostream>
+#include<string>
+#include<list>
+using namespace std;
+
+/*
+Створити програму з використанням патерну Спостерігач(Observer).
+Предмет спостереження – курс долару(чи євро). Спостерігачі – клієнти(банк, брокер). При
+збільшенні(зменшенні) курсу спостерігачі продають(купляють) валюту.
+*/
+
+
+class Iobserver {
+public:
+	virtual ~Iobserver() {};
+	virtual void Update(const string &message_from_subject) = 0;
+};
+
+class Imessage {
+public:
+	virtual ~Imessage() {};
+	virtual void Attach(Iobserver *observer) = 0;
+	virtual void Detach(Iobserver *observer) = 0;
+	virtual void Notify() = 0;
+};
+
+class Message : public Imessage {
+private:
+	list<Iobserver *> list_observer_;
+	string message_;
+public:
+	virtual ~Message() {
+		cout << " Goodbye, I was the Subject.\n";
+	}
+	void Attach(Iobserver *observer) override {
+		list_observer_.push_back(observer);
+	}
+	void Detach(Iobserver *observer) override {
+		list_observer_.remove(observer);
+	}
+	void Notify() override {
+		list<Iobserver *>::iterator iterator = list_observer_.begin();
+		HowManyObserver();
+		while (iterator != list_observer_.end()) {
+			(*iterator)->Update(message_);
+			++iterator;
+		}
+	}
+	void CreateMessage(std::string message = "Empty") {
+		this->message_ = message;
+		Notify();
+	}
+	void HowManyObserver() {
+		cout << " There are " << list_observer_.size() << " observers in the list.\n";
+	}	
+	void SomeBusinessLogic() {
+		this->message_ = "change message message";
+		Notify();
+		cout << " I'm about to do some thing important\n";
+	}
+
+};
+
+class Observer : public Iobserver {
+private:
+	string message_from_subject_;
+	Message &message_;
+	static int static_number;
+	int number;
+
+public:
+	Observer(Message &message) : message_(message) {
+		this->message_.Attach(this);
+		cout << " Hi i'm the Observer" << ++Observer::static_number << endl;
+		this->number = Observer::static_number;
+	}
+	virtual ~Observer() {};
+
+	void Update(const string &message_from_subject) override{
+		message_from_subject_ = message_from_subject;
+		Print();
+	}
+
+	void Remove_me_from_the_list(){
+		message_.Detach(this);
+		cout << " Observer " << number << " remove from list" << endl;
+	}
+
+	void Print() {
+		cout << " Observer " << this->number << " a new message is available -> " << this->message_from_subject_ << endl;
+	}
+};
+
+int Observer::static_number = 0;
+
+int main()
+{
+
+	Message *message = new Message;
+
+	Observer *observer1 = new Observer(*message);
+
+	message->CreateMessage("30.3 $");
+
+
+
+	delete observer1;
+	delete message;
+
+
+	system("pause");
+	return 0;
+}
