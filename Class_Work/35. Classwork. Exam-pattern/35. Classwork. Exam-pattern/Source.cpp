ee015f61331b1a3422e4a03978fa077abd551af9
AntonKozyar <kozyar.ant@gmail.com>
2020-02-23 19:58:56 +0200

    02.23.20

--- Class_Work/35. Classwork. Exam-pattern/35. Classwork. Exam-pattern/Source.cpp
+++ Class_Work/35. Classwork. Exam-pattern/35. Classwork. Exam-pattern/Source.cpp
@@ -13,37 +13,19 @@ using namespace std;
 */
 
 class Task {
+	
 public:
+	int number;
 	string task;
 	string priority;
 	string mark;
 	string deth_line;
-
-public:
-
-	vector<Task> my_list;
-
-	/*void Add_to_list() {
-		for (size_t i = 0; i < my_list.size(); i++) {
-			if (my_list[i] == my_list.back()) {
-				cout << my_list[i];
-			}
-			else {
-				cout << my_list[i] << ", ";
-			}
-		}
-		cout << "\n\n";
-	}*/
-
-	void Add_to_list(Task *task) {
-		my_list.push_back(*task);
-	}
-
 };
+
+vector<Task> my_list;
 
 class Builder {
 public:
-
 	virtual ~Builder() {}
 	virtual void SET_task(string task = "-") = 0;
 	virtual void SET_priority(string priority = "-") = 0;
@@ -51,10 +33,10 @@ public:
 	virtual void SET_deth_line(string deth_line = "-") = 0;
 };
 
-class Task_builder : Builder {
+class Task_builder : public Builder {
 private:
 	Task *my_task;
-	vector<Task> my_list;
+	
 public:
 
 	Task_builder() { this->Reset(); }
@@ -64,7 +46,7 @@ public:
 	}
 
 	void SET_task(string task = "-") override {
-		this->my_task->task = task;
+		this->my_task->task = task;		
 	};
 
 	void SET_priority(string priority = "-")override {
@@ -73,45 +55,16 @@ public:
 
 	void SET_mark(string mark = "-") override {
 		this->my_task->mark = mark;
+		
 	};
 
 	void SET_deth_line(string deth_line = "-") override {
 		this->my_task->deth_line = deth_line;
-	};
-
-	/*void Write_to_file(Task *task) {
-		string path = "data.txt";
-		ofstream inFile;
-
-		inFile.open(path);
-		inFile << this->my_task << endl;
-
-		inFile.close();
-	}*/
+	};	
 
 	void Create_task() {
-		
+		my_task->number = my_list.size()+1;
 		my_list.push_back(*my_task);
-		
-	}
-	void Show_vector() {
-
-		for (int i = 0; i < my_list.size(); i++) {
-			cout << "TASK      : "<< my_task[i].task << endl;
-			cout << "DETH LINE :" << my_task[i].deth_line << endl;
-			cout << "PRIORITY  :" << my_task[i].priority << endl;
-			cout << "MARK      :" << my_task[i].mark << endl;
-			cout << "---------------------------" << endl;
-		}
-	}
-
-
-
-	void Show() {
-		cout << "TASK      : "<< my_task->task << endl;
-		cout << "DETH LINE :" << my_task->deth_line << endl;
-		cout << "PRIORITY  :" << my_task->priority << endl;
-		cout << "MARK      :" << my_task->mark << endl;
 	}
 
 	Task *Get_task() {
@@ -120,6 +73,7 @@ public:
 			return result;
 	}
 
+	~Task_builder(){}
 };
 
 class Directoc_task {
@@ -130,20 +84,24 @@ public:
 	void Set_builder(Task_builder *builder) { this->builder = builder; }
 
 	void Build_draft(string task /*string deth_linestring, string priority, string mark,*/) {
-		this->builder->SET_task(task);
-		this->builder->Create_task();
-
+		this->builder->SET_task(task);	
+		this->builder->SET_deth_line();
+		this->builder->SET_priority();
+		this->builder->SET_mark();
 	}
 
 	void Build_elementary_task(string task, string deth_line/*string priority, string mark,*/ ) {
 		this->builder->SET_task(task);
 		this->builder->SET_deth_line(deth_line);
+		this->builder->SET_priority();
+		this->builder->SET_mark();
 	}
 
 	void Build_priority_task(string task, string deth_line, string priority /*string mark,*/) {
 		this->builder->SET_task(task);		
 		this->builder->SET_deth_line(deth_line);
 		this->builder->SET_priority(priority);
+		this->builder->SET_mark();
 	}
 
 	void Build_full_task(string task, string deth_line, string priority, string mark) {
@@ -153,64 +111,286 @@ public:
 		this->builder->SET_mark(mark);
 	}
 
+	void Create() {		this->builder->Create_task();	}
+
 	void Show() {
-		this->builder->Show();
+		cout << "SIZE : " << my_list.size() << endl;
+
+		for (int i = 0; i < my_list.size(); i++) {
+			cout << "# " << my_list[i].number << endl;
+			cout << "TASK      : "<< my_list[i].task << endl;
+			cout << "DETH LINE : " << my_list[i].deth_line << endl;
+			cout << "PRIORITY  : " << my_list[i].priority << endl;
+			cout << "MARK      : " << my_list[i].mark << endl;
+			cout << "---------------------------" << endl;
+		}
 	}
 
 	void Write_to_file() {
 		string path = "data.txt";
 		ofstream inFile;
-
 		inFile.open(path);
-		inFile << this->builder << endl;
-
+		inFile << my_list.size() << endl;
+		for (int i = 0; i < my_list.size(); i++) {
+			inFile << my_list[i].number << endl;
+			inFile << my_list[i].task << endl;
+			inFile << my_list[i].deth_line << endl;
+			inFile << my_list[i].priority << endl;
+			inFile << my_list[i].mark << endl;
+		}
 		inFile.close();
 	}
 
 	void Serch() {
+		int choise;
+		string tmp;
+		cout << "\n\t====SERCH====\n [1] By date.\n [2] By teg.\n [3] By priority. " << endl;
+		cout << "\n Enter your choise : ";
+		cin >> choise;
 
+		switch (choise)
+		{
+		case 1:
+			cout << "\n Enter date /00.00.00/ : ";
+			cin >> tmp;
+			for (int i = 0; i < my_list.size(); i++) {
+				if (tmp == my_list[i].deth_line) {					
+					cout << "\n    TASK      : " << my_list[i].task << endl;		
+					cout << "->> DETH LINE : " << my_list[i].deth_line << endl;
+					cout << "    PRIORITY  : " << my_list[i].priority << endl;
+					cout << "    MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+				}
+			}			
+			break;
+		case 2:
+			cout << "\n Enter teg /#teg/ : ";
+			cin >> tmp;
+			for (int i = 0; i < my_list.size(); i++) {
+				if (tmp == my_list[i].mark) {
+					cout << "\n    DETH LINE : " << my_list[i].deth_line << endl;
+					cout << "    TASK      : " << my_list[i].task << endl;
+					cout << "    PRIORITY  : " << my_list[i].priority << endl;
+					cout << "->> MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+				}
+			}
+			break;
+		case 3: 
+			cout << "\n Enter priority /string/ : ";
+			cin >> tmp;
+			for (int i = 0; i < my_list.size(); i++) {
+				if (tmp == my_list[i].priority) {
+					cout << "\n   DETH LINE : " << my_list[i].deth_line << endl;
+					cout << "    TASK      : " << my_list[i].task << endl;
+					cout << "->> PRIORITY  : " << my_list[i].priority << endl;
+					cout << "    MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+				}
+			}
+			break;
+		default:
+			cout << " Wrong choise." << endl;
+			break;
+		}
 	}
 
+	void Change_task() {
+		int choise;
+		int choise2;
+		string tmp;
+		cout << "\n Select a task : " << endl;
+		Show();
+
+		cout << "\n Enter your choise : ";
+		cin >> choise;
+	
+
+		for (int i = 0; i < my_list.size(); i++) {
+			if (choise == my_list[i].number) {
+
+				cout << "\n [1] Task. [2] Data. [3] Priority. [4] Mark. [5] All task.";
+				cout << "\n Enter what you want to change : ";
+				cin >> choise2;
+				switch (choise2)
+				{
+				case 1:
+					cout << " Enter new task : ";
+					cin >> tmp;
+					my_list[i].task = tmp;
+
+					cout << "---------------------------" << endl;
+					cout << "\n # " << my_list[i].number << endl;
+					cout << " TASK      : " << my_list[i].task << endl;
+					cout << " DETH LINE : " << my_list[i].deth_line << endl;
+					cout << " PRIORITY  : " << my_list[i].priority << endl;
+					cout << " MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+
+					break;
+				case 2:
+					cout << " Enter new deth line : ";
+					cin >> tmp;
+					my_list[i].deth_line = tmp;
+
+					cout << "---------------------------" << endl;
+					cout << "\n # " << my_list[i].number << endl;
+					cout << " TASK      : " << my_list[i].task << endl;
+					cout << " DETH LINE : " << my_list[i].deth_line << endl;
+					cout << " PRIORITY  : " << my_list[i].priority << endl;
+					cout << " MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+
+					break;
+				case 3:
+					cout << " Enter new priority : ";
+					cin >> tmp;
+					my_list[i].priority = tmp;
+
+					cout << "---------------------------" << endl;
+					cout << "\n # " << my_list[i].number << endl;
+					cout << " TASK      : " << my_list[i].task << endl;
+					cout << " DETH LINE : " << my_list[i].deth_line << endl;
+					cout << " PRIORITY  : " << my_list[i].priority << endl;
+					cout << " MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+
+					break;
+				case 4:
+					cout << " Enter new mark : ";
+					cin >> tmp;
+					my_list[i].mark = tmp;
+
+					cout << "---------------------------" << endl;
+					cout << "\n # " << my_list[i].number << endl;
+					cout << " TASK      : " << my_list[i].task << endl;
+					cout << " DETH LINE : " << my_list[i].deth_line << endl;
+					cout << " PRIORITY  : " << my_list[i].priority << endl;
+					cout << " MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+
+					break;
+				case 5:
+					break;
+					cout << " Enter new task : ";
+					cin >> tmp;
+					my_list[i].task = tmp;
+					cout << " Enter new deth line : ";
+					cin >> tmp;
+					my_list[i].deth_line = tmp;
+					cout << " Enter new priority : ";
+					cin >> tmp;
+					my_list[i].priority = tmp;
+					cout << " Enter new mark : ";
+					cin >> tmp;
+					my_list[i].mark = tmp;
+
+					cout << "---------------------------" << endl;
+					cout << "\n # " << my_list[i].number << endl;
+					cout << " TASK      : " << my_list[i].task << endl;
+					cout << " DETH LINE : " << my_list[i].deth_line << endl;
+					cout << " PRIORITY  : " << my_list[i].priority << endl;
+					cout << " MARK      : " << my_list[i].mark << endl;
+					cout << "---------------------------" << endl;
+
+				default:
+					break;
+				}				
+			}			
+		}
+	}
+
+	void Delete_task() {
+		int choise;
+		cout << "\n Select a task : \n\t";		
+		Show();
+		cout << " Enter choise : ";
+		cin >> choise;
+
+		for (int i = 0; i<my_list.size(); i++) {
+			if ((choise-1) == i) {
+				my_list.erase(my_list.begin() + i);
+			}
+		}		
+	}
+
+	void Read_from_file() {
+		Task my_task;
+		ifstream readFile;
+		char temp[255];
+			
+
+		readFile.open("data.txt");
+
+		if (!readFile.is_open()) {
+			cout << "Can't open file!\n";
+		}
+		else {
+			int size = 0, i = 0;
+			readFile.getline(temp, 255);
+			size = atoi(temp);
+			while (i < size) {
+
+				readFile.getline(temp, 255);
+				my_task.number = atoi(temp);
+
+				readFile.getline(temp, 255);
+				my_task.task = temp;
+
+				readFile.getline(temp, 255);
+				my_task.deth_line = temp;
+
+				readFile.getline(temp, 255);
+				my_task.priority = temp;
+
+				readFile.getline(temp, 255);
+				my_task.mark = temp;
+
+				my_list.push_back(my_task);
+				i++;
+			}
+		}
+		readFile.close();		
+	}
 };
 
 class Facade {
 
 };
 
-void ClientCode(Directoc_task& director)
+void ClientCode(Directoc_task &director)
 {
-	Task_builder* builder = new Task_builder();
+
+	/*Task_builder* builder = new Task_builder();
 	director.Set_builder(builder);
-	cout << "Standard basic product:\n";
 	director.Build_draft("HELLO");
-	director.Show();
+	director.Create();
+
+	Task_builder* builder2 = new Task_builder();
+	director.Set_builder(builder2);
+	director.Build_draft("HELLO2");
+	director.Create();
+
+	Task_builder* builder3 = new Task_builder();
+	director.Set_builder(builder3);
+	director.Build_full_task("1", "2", "3", "4");
+	director.Create();*/
+
+
+	/*director.Show();
 	director.Write_to_file();
 
-	/*builder->SET_deth_line("12316");
-	builder->Show();*/
+	director.Serch();
 
-	//Product1* p = builder->GetProduct();
-	//p->ListParts();
-	//delete p;
+	director.Change_task();
+*/
+	//director.Delete_task();
+	//director.Write_to_file();
+	director.Read_from_file();
+	director.Show();
 
-	//std::cout << "Standard full featured product:\n";
-	//director.BuildFullFeaturedProduct();
-
-	//p = builder->GetProduct();
-	//p->ListParts();
-	//delete p;
-
-	//// Помните, что паттерн Строитель можно использовать без класса Директор.
-	/*std::cout << "Custom product:\n";
-	builder->SET_task("HEllo");*/
-
-
-	/*builder->ProducePartC();
-	p = builder->GetProduct();
-	p->ListParts();
-	delete p;*/
-
-	delete builder;
+	/*delete builder;
+	delete builder2;*/
 }
 
 int main() {
